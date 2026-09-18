5d0ebcf6873f17520da8269153495d64c9ce0c79
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-18 22:19:27 +0100

    Point sidebar My Work to shared work runtime

--- app/src/lib/components/FunctionSidebar.svelte
+++ app/src/lib/components/FunctionSidebar.svelte
@@ -12,7 +12,7 @@
   <section>
     <p class="label">Workspace</p>
     <a class="utility active" href={`/${tenantSlug}/app/functions/f01`}>⌂ <span>Home</span></a>
-    <a class="utility" href={`/${tenantSlug}/app/functions/f01`}>▣ <span>My work</span></a>
+    <a class="utility" class:active={page.url.pathname.includes('/app/work')} href={`/${tenantSlug}/app/work`}>▣ <span>My work</span></a>
     <a class="utility" href={`/${tenantSlug}/app/functions/f01`}>◌ <span>Notifications</span></a>
   </section>
 

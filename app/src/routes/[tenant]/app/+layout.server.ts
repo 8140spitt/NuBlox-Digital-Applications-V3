a60cb3851e16c35fe1d88f7b68321eca6e61b4c4
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-18 20:26:52 +0100

    db: await MySQL tenant context in app layout

--- app/src/routes/[tenant]/app/+layout.server.ts
+++ app/src/routes/[tenant]/app/+layout.server.ts
@@ -1,8 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 import { resolveDevelopmentCommandContext } from '$lib/server/platform-context';
 
-export const load: LayoutServerLoad = ({ params }) => {
-  const context = resolveDevelopmentCommandContext(params.tenant);
+export const load: LayoutServerLoad = async ({ params }) => {
+  const context = await resolveDevelopmentCommandContext(params.tenant);
   return {
     tenantSlug: context.tenantSlug,
     actorDisplayName: context.actorDisplayName,

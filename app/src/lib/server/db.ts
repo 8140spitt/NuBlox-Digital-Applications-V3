12aaeff2326979d498ef46d20741e1b51a42335f
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-19 01:09:11 +0100

    Require Governance Body migration

--- app/src/lib/server/db.ts
+++ app/src/lib/server/db.ts
@@ -6,7 +6,7 @@ import mysql, {
   type RowDataPacket
 } from 'mysql2/promise';
 
-export const requiredMigration = '0024_scenario_runtime.sql';
+export const requiredMigration = '0025_governance_body_runtime.sql';
 
 function databaseUrl() {
   if (

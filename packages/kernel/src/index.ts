80c21d11a28714c7d194fbe74d7dce9ed7d7959a
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-20 20:50:41 +0100

    kernel: export construction industry solution model

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -1,3 +1,5 @@
+export * from './industry.js';
+export * from './industry-factories.js';
 export * from './my-work.js';
 export * from './deliverables.js';
 export * from './deliverable-factories.js';

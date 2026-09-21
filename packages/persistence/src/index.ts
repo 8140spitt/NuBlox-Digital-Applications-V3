e511bdaac62181426eed5134d7d773700dea556b
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-21 23:03:41 +0100

    feat(deliverables): export read projection

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -1,3 +1,4 @@
+export * from './deliverable-read-repository.js';
 export * from './change-configuration-command-service.js';
 export * from './change-configuration-read-repository.js';
 export * from './information-command-service.js';

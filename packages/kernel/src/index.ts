ef54a32b465750a2d6ba6645f8d61350d448b994
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-23 20:43:06 +0100

    feat: export configuration resolution model

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -28,6 +28,8 @@ export * from './validation-factories.js';
 export * from './validation-execution.js';
 export * from './metadata.js';
 export * from './metadata-factories.js';
+export * from './configuration-resolution.js';
+export * from './configuration-resolution-factories.js';
 export * from './control.js';
 export * from './control-factories.js';
 export * from './errors.js';

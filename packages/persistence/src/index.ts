5037187de7623d788cb3554c5e28688ed4410eb6
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-23 20:49:37 +0100

    feat: export configuration resolution read projection

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -45,6 +45,7 @@ export * from './metadata-administration-command-service.js';
 export * from './metadata-administration-read-repository.js';
 export * from './configuration-resolution-repository.js';
 export * from './configuration-resolution-command-service.js';
+export * from './configuration-resolution-read-repository.js';
 export * from './control-repository.js';
 export * from './database.js';
 export * from './migrations.js';

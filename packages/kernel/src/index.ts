ebaeac7f3bf303fb150bea7ae051799cd666f4d7
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-24 19:35:00 +0100

    feat: expose organisational resource planning kernel

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -11,6 +11,8 @@ export * from './exchange.js';
 export * from './exchange-factories.js';
 export * from './functional.js';
 export * from './functional-factories.js';
+export * from './organisational-context.js';
+export * from './organisational-context-factories.js';
 export * from './portability.js';
 export * from './portability-factories.js';
 export * from './publication.js';

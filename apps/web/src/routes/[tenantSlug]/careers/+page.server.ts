33d9f32d8ee7907b8c412bd8a6c04f9cc84c943d
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-25 02:08:30 +0100

    fix: remove implicit tenant public route

--- apps/web/src/routes/[tenantSlug]/careers/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { requirePublicTenant } from '$lib/server/public-tenant';
-
-export const load: PageServerLoad = async ({ params }) => ({
-  tenant: await requirePublicTenant(params.tenantSlug)
-});

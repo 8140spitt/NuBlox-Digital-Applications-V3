2221d718ef32c0854b65c627e06950d725f8a88a
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-25 02:14:44 +0100

    refactor: remove app reroute shim

--- apps/web/src/hooks.ts
+++
@@ -1,4 +0,0 @@
-import type { Reroute } from '@sveltejs/kit';
-import { rerouteTenantApplicationPath } from '$lib/tenant-paths';
-
-export const reroute: Reroute = ({ url }) => rerouteTenantApplicationPath(url.pathname);

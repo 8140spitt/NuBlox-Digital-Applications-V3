ac621c0acc96c83cf118c7860ba1bb90911a370e
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-09-25 01:52:25 +0100

    feat: export tenant routing repository

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -59,6 +59,7 @@ export * from './control-repository.js';
 export * from './database.js';
 export * from './migrations.js';
 export * from './repository.js';
+export * from './tenant-routing-repository.js';
 
 export * from './extension-repository.js';
 export * from './extension-command-service.js';
